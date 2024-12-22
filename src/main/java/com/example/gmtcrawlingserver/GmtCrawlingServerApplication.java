d829a703181363b30d98b26b620aabf7187aad4d
saul <dlathf3210@naver.com>
2024-12-22 18:57:23 +0900

    [START]

---
+++ src/main/java/com/example/gmtcrawlingserver/GmtCrawlingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmtcrawlingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmtCrawlingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmtCrawlingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gmtcrawlingserver/GmtCrawlingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmtcrawlingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmtCrawlingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
